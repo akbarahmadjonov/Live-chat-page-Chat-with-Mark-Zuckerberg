8f62d74f33c249b269389be42714c7ff6beb680a
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-10-24 13:28:10 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let x = document.getElementById('chat');
+    if (x.style.display === 'block') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+// Has been occured
